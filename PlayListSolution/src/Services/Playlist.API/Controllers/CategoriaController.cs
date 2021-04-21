80341aa572f84e56c06fedebc5112214e18ded30
MMarcos <mauricio.marcos@castgroup.com.br>
2021-04-21 17:25:23 -0300

    Criado a API CategoriaController com serviço "buscarTodos"

--- PlayListSolution/src/Services/Playlist.API/Controllers/CategoriaController.cs
+++ PlayListSolution/src/Services/Playlist.API/Controllers/CategoriaController.cs
@@ -1,10 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Playlist.API.Domain.Interfaces.Service;
+using Playlist.API.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Playlist.API.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
     public class CategoriaController : ControllerBase
     {
+        private readonly ICategoriaService<CategoriaViewModel> _service;
+
+        public CategoriaController(ICategoriaService<CategoriaViewModel> categoriaService)
+        {
+            _service = categoriaService;
+        }
+
+        [HttpGet("buscarTodos")]
+        [ProducesResponseType(typeof(IEnumerable<CategoriaViewModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var response = await _service.BuscarTodos();
+                if (response.Count() is 0) return NoContent();
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    MensagemErro = ex.Message,
+                    StakTraceErro = ex.StackTrace
+                });
+            }
+        }
     }
 }
